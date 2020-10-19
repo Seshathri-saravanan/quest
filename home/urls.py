aab24e49bfc702980f3bc4343a70c7eb800816ab
seshathri saravanan <seshathrisaravanan@gmail.com>
2020-10-19 17:38:41 +0530

    added images

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('historical',views.historical,name='historical')
+    path('historical',views.historical,name='historical'),
+    path('temples',views.temples,name='temples'),
+    path('parks',views.parks,name='parks'),
+    path('cities',views.cities,name='cities'),
+    path('beaches',views.beaches,name='beaches'),
+    path('hills',views.hills,name='hills')
 ]
--- home/views.py
+++ home/views.py
@@ -6,4 +6,20 @@ def index(request):
     return render(request, 'home/landingpage.html', {})
 
 def historical(request):
-    return render(request, 'places/place.html', {})
+    return render(request, 'places/historical.html', {'placename':"HISTORICAL PLACES"})
+
+def cities(request):
+    return render(request, 'places/cities.html', {'placename':"MAJOR CITIES"})
+
+def parks(request):
+    return render(request, 'places/parks.html', {'placename':"NATIONAL PARKS"})
+
+def beaches(request):
+    return render(request, 'places/beaches.html', {'placename':"BEACHES"})
+
+def temples(request):
+    return render(request, 'places/temples.html', {'placename':"TEMPLES"})
+
+def hills(request):
+    return render(request, 'places/hills.html', {'placename':"HILL STATIONS"})
+
